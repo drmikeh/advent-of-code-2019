b63ebbbb2c958633afd6e23c3c6091e4782e2d1e
Dr. Mike Hopper <michael_a_hopper@homedepot.com>
2019-12-30 12:56:12 -0500

    Adds readme files for days 7 and 8

--- day11/part2.js
+++ day11/part2.js
@@ -4,24 +4,12 @@ const paint = require('./paint')
 const computer = makeComputer('data.txt')
 const panelsPainted = paint(computer)
 
-const keys = Object.keys(panelsPainted)
+const keys = Object.keys(panelsPainted).map(key => key.split(','))
 
-const minX = keys.reduce(
-    (min, panelKey) => Math.min(min, panelKey.split(',')[0]),
-    +999
-)
-const maxX = keys.reduce(
-    (max, panelKey) => Math.max(max, panelKey.split(',')[0]),
-    -999
-)
-const minY = keys.reduce(
-    (min, panelKey) => Math.min(min, panelKey.split(',')[1]),
-    +999
-)
-const maxY = keys.reduce(
-    (max, panelKey) => Math.max(max, panelKey.split(',')[1]),
-    -999
-)
+const minX = keys.reduce((min, panelKey) => Math.min(min, panelKey[0]), +999)
+const maxX = keys.reduce((max, panelKey) => Math.max(max, panelKey[0]), -999)
+const minY = keys.reduce((min, panelKey) => Math.min(min, panelKey[1]), +999)
+const maxY = keys.reduce((max, panelKey) => Math.max(max, panelKey[1]), -999)
 
 for (let y = minY; y <= maxY; y++) {
     let line = ''
